a51a9c82991d70d0a0871826026532ada1c99238
Lívia Vieira Vasconcelos <12471757669@MUZAMBINHO.EDU.BR>
2026-06-15 10:11:40 -0300

    Finalizei o Backend e inseri os Inlines

--- app/admin.py
+++ app/admin.py
@@ -1,14 +1,28 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Categoria)
-admin.site.register(Usuario)
+# INLINE 1
+class ItemPerdidoInline(admin.TabularInline):
+    model = ItemPerdido
+    extra = 0
+
+# INLINE 2
+class DevolucaoInline(admin.TabularInline):
+    model = Devolucao
+    extra = 0
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    inlines = [ItemPerdidoInline]
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    inlines = [DevolucaoInline]
+
 admin.site.register(StatusItem)
 admin.site.register(LocalEncontrado)
-admin.site.register(ItemPerdido)
 admin.site.register(ItemEncontrado)
 admin.site.register(ConsultaItem)
-admin.site.register(Devolucao)
 admin.site.register(AtualizacaoItem)
 admin.site.register(ExcluirItem)
 admin.site.register(HistoricoDevolucao)
